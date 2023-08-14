b20b8706211fa7c9a448f06a792fc7d818f4e8fc
Jackson24391 <131276854+Jackson24391@users.noreply.github.com>
2023-08-14 18:44:46 -0400

    commit

--- MySpotter/server.js
+++ MySpotter/server.js
@@ -1,17 +1,41 @@
+require('dotenv').config();
+const express = require('express');
+const session = require('express-session');
+const expressHandlebars = require('express-handlebars');
+const path = require('path');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const viewRoutes = require('./routes/viewRoutes');
+const sequelize = require('./config/connection');
 const userRoutes = require('./routes/userRoutes');
 const exerciseRoutes = require('./routes/exerciseRoutes');
 const workoutRoutes = require('./routes/workoutRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const sess = {
+  secret: process.env.SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+const hbs = expressHandlebars.create({ defaultLayout: 'main' });
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/users', userRoutes);
 app.use('/api/exercises', exerciseRoutes);
 app.use('/api/workouts', workoutRoutes);
 app.use('/', viewRoutes);
-
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}!`);
-  });
-});
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
